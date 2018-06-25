b9c93027ae5e3126cb6d7216475127d137982058
Dingzhenchuan <zhenchuanding@sina.com>
2018-06-25 13:28:40 +0800

    reverse str II

--- String data structure.py
+++ String data structure.py
@@ -7,7 +7,24 @@ class Solution(object):
         """
         return s[::-1]
 
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        indx=len(s)//k
+        for i in range(indx+1):
+            if i%2==0:
+                s=s[:i*k]+s[i*k:(i+1)*k][::-1]+s[(i+1)*k:]
+        return s
+        
+
 
 if __name__=="__main__":
     sol=Solution()
     print(sol.reverseString('hello'))
+    s="hyzqyljrnigxvdtneasepfahmtyhlohwxmkqcdfehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjqlimjkfnqcqnajmebeddqsgl"
+    k=39
+    m=sol.reverseStr(s,k)
+    print(m)
