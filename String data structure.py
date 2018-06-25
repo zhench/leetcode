27fd331220b13965dd637dd13915cb977a7b3bae
Dingzhenchuan <zhenchuanding@sina.com>
2018-06-25 13:50:27 +0800

    reverse words

--- String data structure.py
+++ String data structure.py
@@ -18,7 +18,16 @@ class Solution(object):
             if i%2==0:
                 s=s[:i*k]+s[i*k:(i+1)*k][::-1]+s[(i+1)*k:]
         return s
-        
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res=[]
+        lst=s.split()
+        for l in lst:
+            res.append(l[::-1])
+        return ' '.join(res)        
 
 
 if __name__=="__main__":
@@ -28,3 +37,5 @@ if __name__=="__main__":
     k=39
     m=sol.reverseStr(s,k)
     print(m)
+    s='abc def ghi'
+    print(sol.reverseWords(s))
