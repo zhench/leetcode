8c4dfaf7f6def497843168c7be7968e7c5f3c2c2
Dingzhenchuan <zhenchuanding@sina.com>
2018-06-25 15:49:45 +0800

    is Anagram

--- String data structure.py
+++ String data structure.py
@@ -13,29 +13,53 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        indx=len(s)//k
-        for i in range(indx+1):
-            if i%2==0:
-                s=s[:i*k]+s[i*k:(i+1)*k][::-1]+s[(i+1)*k:]
+        indx = len(s) // k
+        for i in range(indx + 1):
+            if i % 2 == 0:
+                s = s[:i * k] + s[i * k:(i + 1) * k][::-1] + s[(i + 1) * k:]
         return s
+
     def reverseWords(self, s):
         """
         :type s: str
         :rtype: str
         """
-        res=[]
-        lst=s.split()
+        res = []
+        lst = s.split()
         for l in lst:
             res.append(l[::-1])
-        return ' '.join(res)        
+        return ' '.join(res)
+
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        i = len(t) - 1
+        while i >= 0:
+            tchar = t[i]
+            if tchar in s:
+                s=s.replace(tchar, '', 1)
+                i -= 1
+            else:
+                return False
+        if len(s) == 0:
+            return True
+        return False
 
 
-if __name__=="__main__":
-    sol=Solution()
+if __name__ == "__main__":
+    sol = Solution()
     print(sol.reverseString('hello'))
-    s="hyzqyljrnigxvdtneasepfahmtyhlohwxmkqcdfehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjqlimjkfnqcqnajmebeddqsgl"
-    k=39
-    m=sol.reverseStr(s,k)
-    print(m)
-    s='abc def ghi'
-    print(sol.reverseWords(s))
+    s = "hyzqyljrnigxvdtneasepfahmtyhlohwxmkqcdfehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjqlimjkfnqcqnajmebeddqsgl"
+    k = 39
+    m = sol.reverseStr(s, k)
+    # print(m)
+    s = 'abc def ghi'
+    # print(sol.reverseWords(s))
+    s = "anagram"
+    m = "nagaram"
+    print(sol.isAnagram(s,m))
